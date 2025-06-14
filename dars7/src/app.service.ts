c29b1ba0b37be41ca5ebf2aad25e9703830f692f
Fayzillo Ummatov <ovovovlululutvata@gmail.com>
2025-06-14 20:04:37 +0000

    First log adding

--- dars7/src/app.service.ts
+++ dars7/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return   `
+      <h1> 🚀 Application is running on: http://localhost:3000 </h1>
+    `;
   }
 }
